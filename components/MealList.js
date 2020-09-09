ada7cea1adf5acdd6eb627c7a2f629d09d5aa1da
Andrei Pirea <andrei.pirea@hogarthww.com>
2020-09-09 15:29:12 +0300

    app done

---
+++ components/MealList.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { useSelector } from 'react-redux';
+import MealItem from './MealItem';
+
+const MealList = (props) => {
+  const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
+
+  const renderMealItem = (itemData) => {
+    const isFavorite = favoriteMeals.some(meal => meal.id === itemData.item.id);
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({routeName: 'MealDetail', params: {
+            mealId: itemData.item.id,
+            mealTitle: itemData.item.title,
+            isFav: isFavorite,
+          }})
+        }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.list}>
+      <FlatList
+        data={props.listData}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
+
+      {/* <Text>the category meals screen</Text>
+  <Text>{selectedCategory.title}</Text>
+  <Button
+    title="Go to Detaild!"
+    onPress={() => {
+      props.navigation.navigate({
+        routeName: 'MealDetail',
+      });
+    }}
+  /> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    padding: 10,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealList;
---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSelector } from 'react-redux';
+
+import { CATEGORIES } from "../data/dummy-data";
+import MealList from "../components/MealList";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaltText";
+
+const CatergoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+
+  const availableMeals = useSelector(state => state.meals.filteredMeals);
+
+  const displayMeals = availableMeals.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  if(displayMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={displayMeals} navigation={props.navigation} />;
+};
+
+CatergoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default CatergoryMealsScreen;
